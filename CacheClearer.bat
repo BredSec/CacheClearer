346076881ce3205364b344117740aeb2862c93a4
BredAndButter <55727390+BredAndButter@users.noreply.github.com>
2022-11-18 15:35:00 -0500

    Update CacheClearer.bat
    Added clearing of old driver files

--- CacheClearer.bat
+++ CacheClearer.bat
@@ -17,8 +17,17 @@ for /d %%p in ("C:\Windows\Temp\*.*") do rmdir "%%p" /s /q
 del /s /q "C:\Windows\SoftwareDistribution\Download\*.*"
 for /d %%p in ("C:\Windows\SoftwareDistribution\Download\*.*") do rmdir "%%p" /s /q
 
-del /s /q "C:\Windows\Prefetch\*.*"
-for /d %%p in ("C:\Windows\Prefetch\*.*") do rmdir "%%p" /s /q
+del /s /q "%WinDir%\Prefetch\*.*"
+for /d %%p in ("%WinDir%\Prefetch\*.*") do rmdir "%%p" /s /q
+
+del /s /q "%SYSTEMDRIVE%\AMD\*.*"
+for /d %%p in ("%SYSTEMDRIVE%\AMD\*.*") do rmdir "%%p" /s /q
+
+del /s /q "%SYSTEMDRIVE%\NVIDIA\*.*"
+for /d %%p in ("%SYSTEMDRIVE%\NVIDIA\*.*") do rmdir "%%p" /s /q
+
+del /s /q "%SYSTEMDRIVE%\INTEL\*.*"
+for /d %%p in ("%SYSTEMDRIVE%\INTEL\*.*") do rmdir "%%p" /s /q
 
 cd "C:\Windows\system32\"
 cleanmgr.exe
